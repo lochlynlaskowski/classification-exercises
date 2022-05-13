689697cdca5a9399736605cb9cb899c849251df1
lochlynlaskowski <lochlyn.laskowski@gmail.com>
2022-05-12 22:02:39 -0500

    added classification exercises

--- acquire.py
+++ acquire.py
@@ -1,18 +1,69 @@
 import pandas as pd
+import numpy as np
 import os
 from env import get_db_url
-
+    
+    
 def get_titanic_data():
-    filename = "titanic.csv"
+    filename = 'titanic_data.csv'
 
     if os.path.isfile(filename):
         return pd.read_csv(filename)
     else:
-        url = get_db_url("titanic")
-        query = "select * from customers"
+       sql_query = 'SELECT * FROM passengers'
+       
+    
+    df = pd.read_sql(sql_query, get_db_url('titanic_db'))
 
-        df = pd.read_sql(query, url)
+    df.to_csv(filename)
 
-        df.to_csv(filename)
+    return df
 
-        return df
+############
+
+def get_iris_data():
+    filename = 'iris_data.csv'
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+       sql_query = """
+                SELECT 
+                    species_id,
+                    species_name,
+                    sepal_length,
+                    sepal_width,
+                    petal_length,
+                    petal_width
+                FROM measurements
+                JOIN species USING(species_id)
+                """
+    
+    df = pd.read_sql(sql_query, get_db_url('iris_db'))
+
+    df.to_csv(filename)
+
+    return df
+
+
+############
+
+
+
+def get_telco_data():
+    filename = 'telco_data.csv'
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+       sql_query = '''SELECT * from customers
+                JOIN contract_types using (contract_type_id)
+                JOIN internet_service_types using (internet_service_type_id)
+                JOIN payment_types using (payment_type_id)'''
+       
+    
+    df = pd.read_sql(sql_query, get_db_url('telco_churn'))
+
+    df.to_csv(filename)
+
+    return df
