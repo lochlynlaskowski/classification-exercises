999288b70e3fede1b5e8fd3a50c8ccd73444075e
lochlynlaskowski <lochlyn.laskowski@gmail.com>
2022-05-14 12:00:18 -0500

    added additional work

--- perpare.py
+++ perpare.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+
+
 def prep_iris(df):
     columns_to_drop = ['species_id', 'Unnamed: 0', 'measurement_id']
     df = df.drop(columns = columns_to_drop)
@@ -5,4 +12,6 @@ def prep_iris(df):
     dummy_df = pd.get_dummies(df_iris[['species']], dummy_na=False, drop_first=[True])
     df = pd.concat([df, dummy_df], axis=1)
     return df
+
+
 
